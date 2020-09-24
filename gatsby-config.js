57137b472f5d43ccf35d6bb01c656fa20df38f48
Zoltan Simon <zoltan@zoltansimon.me>
2020-09-24 22:34:16 +0200

    favicon fix #6

--- gatsby-config.js
+++ gatsby-config.js
@@ -64,7 +64,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/favicon.ico`,
+        icon: `src/favicon.png`,
       },
     },
     {
